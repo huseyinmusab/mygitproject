98e2c2b97972935b3107e17d34845373af440af4
huseyinmusab <huseyinmkocal@gmail.com>
2021-04-09 18:38:11 +0200

    version02

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -4,8 +4,13 @@ public class MyGitClass {
 
 	public static void main(String[] args) {
 
+
 		System.out.println("Version 1");
+		System.out.println("My second commit");
 		
+		///Users/Salak/eclipse-workspace/mygitproject
+		//C:\Users\Salak\eclipse-workspace\mygitproject ===>dosya yontemiyle path
+		//C:\Users\Salak\eclipse-workspace\mygitproject>  ==>sondaki > sil,eclipse terminal ile path bulma
 	}
  
 }
