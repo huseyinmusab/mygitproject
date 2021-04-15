1f0d69fe2c5739c3fb6080d9acd5986939a1ffe6
huseyinmusab <huseyinmkocal@gmail.com>
2021-04-15 15:39:10 +0200

    yeni versiyonum

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -8,6 +8,7 @@ public class MyGitClass {
 		System.out.println("Version 1");
 		System.out.println("My second commit");
 		System.out.println("My third commit");
+		System.out.println("My fourth commit");
 		
 		///Users/Salak/eclipse-workspace/mygitproject
 		//C:\Users\Salak\eclipse-workspace\mygitproject ===>dosya yontemiyle path
