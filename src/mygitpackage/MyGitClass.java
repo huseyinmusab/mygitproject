741514008bfde9128cbc62bc4b7c362af84c4ee9
huseyinmusab <huseyinmkocal@gmail.com>
2021-04-21 14:10:53 +0200

    yerel kodlarim

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -16,7 +16,7 @@ public class MyGitClass {
 		System.out.println("uzak sunucudaki kodlsrim");//GitHub.com daki yeni kodum
 		System.out.println("uzak sunucudaki kodlsrim2");
 		System.out.println("uzak sunucudaki kodlsrim3");
-
+        System.out.println("19. satir bilgisayarim");
 
 		
 		///Users/Salak/eclipse-workspace/mygitproject
